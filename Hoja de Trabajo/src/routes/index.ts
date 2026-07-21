600cb2b0721ec29670ead2a505b1650dd4b0338f
fernandoherrarte-2021072 <fherrarte-2021072@kinal.edu.gt>
2026-07-21 11:38:59 -0600

    feat: conectar rutas de clientes al router principal

--- Hoja de Trabajo/src/routes/index.ts
+++ Hoja de Trabajo/src/routes/index.ts
@@ -1,5 +1,6 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { productosRoute } from "./productos";
+import { clientesRoute } from "./clientes";
 
 export async function router(
     req: IncomingMessage,
@@ -12,6 +13,13 @@ export async function router(
     );
 
     if (productos) return;
+
+    const clientes = await clientesRoute(
+        req,
+        res
+    );
+
+    if (clientes) return;
 
     res.writeHead(404, {
         "Content-Type": "application/json"
