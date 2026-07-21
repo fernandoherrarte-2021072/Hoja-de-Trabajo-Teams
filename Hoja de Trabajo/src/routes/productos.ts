d5733c8457382fed0349a44c45087b2302d783a5
fernandoherrarte-2021072 <fherrarte-2021072@kinal.edu.gt>
2026-07-21 10:32:28 -0600

    feat: implementar ruta get producto por id

--- Hoja de Trabajo/src/routes/productos.ts
+++ Hoja de Trabajo/src/routes/productos.ts
@@ -22,6 +22,52 @@ export async function productosRoute(
         return true;
     }
 
+    if (
+        req.method === "GET" &&
+        req.url?.startsWith("/productos/")
+    ) {
+
+        const id = Number(
+            req.url.split("/")[2]
+        );
+
+        const productos =
+            await obtenerProductos();
+
+        const producto =
+            productos.find(
+                (p: any) => p.id === id
+            );
+
+        if (!producto) {
+
+            res.writeHead(404, {
+                "Content-Type": "application/json"
+            });
+
+            res.end(
+                JSON.stringify({
+                    error:
+                        "Producto no encontrado"
+                })
+            );
+
+            return true;
+
+        }
+
+        res.writeHead(200, {
+            "Content-Type": "application/json"
+        });
+
+        res.end(
+            JSON.stringify(producto)
+        );
+
+        return true;
+
+    }
+
     return false;
 
 }
